01a7401afce0654e75d09532a4a2326a24b34bcd
arysson <arysson@users.noreply.github.com>
2022-02-14 07:44:15 +0000

    deploy: 1f1704c6f20172971c86b7b3f1992ac2cdebbde9

---
+++ precache-manifest.c5a434c9c1be2451791d0ef723817211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed5644e95e9748ae9af03c5121f4d5b",
+    "url": "https://arysson.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "8badac7824851692e246",
+    "url": "https://arysson.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "43b07e28894d53c4b1a6",
+    "url": "https://arysson.github.io/task-management-frontend/static/js/2.7b6c462c.chunk.js"
+  },
+  {
+    "revision": "8badac7824851692e246",
+    "url": "https://arysson.github.io/task-management-frontend/static/js/main.0c57153e.chunk.js"
+  },
+  {
+    "revision": "1cc2777e20235ac2163a",
+    "url": "https://arysson.github.io/task-management-frontend/static/js/runtime~main.5a91e2a3.js"
+  }
+]);
